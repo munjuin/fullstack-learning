733ab6718de78e38bee41b89175b7ab900aaa20c
juin <raiawa1212@gmail.com>
2025-08-20 22:14:17 +0900

    feat: Node.js 라우팅

--- NodeJS-Express-MongoDB/server.js
+++ NodeJS-Express-MongoDB/server.js
@@ -6,5 +6,18 @@ app.listen(8080, ()=>{
 })
 
 app.get('/', (req, res)=>{
-  res.send('하이');
+  // res.send('하이');
+  res.sendFile(__dirname + '/index.html');
+})
+
+// app.get('/news', (req, res)=>{
+//   res.send('뉴스페이지입니다');
+// })
+
+// app.get('/shop', (req, res)=>{
+//   res.send('쇼핑페이지입니다');
+// })
+
+app.get('/about', (req, res)=>{
+  res.sendFile(__dirname + '/about.html');
 })
